0e963c99900e309441ab0d4a0f8fb5ce5c7755d1
drigoalexander <drigosihombinga@gmail.com>
2022-06-11 06:17:38 +0700

    point-user

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="w-full h-auto mx-auto flex flex-col gap-6 max-w-screen-2xl font-poppins bg-gray-200"
+  >
+    <navbar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import navbar from '../components/Navbar.vue'
+export default {
+  components: { navbar },
+}
+</script>
+
+<style></style>
---
+++ pages/NavbarDrop.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="w-screen h-screen flex items-center justify-center mx-auto">
+    <div class="relative flex flex-col items-center justify-center">
+      <nav
+        class="w-full h-full relative select-none cursor-pointer"
+        @click="dropdown"
+      >
+        <div
+          class="w-auto h-auto flex border-2 border-black rounded-lg px-10 py-2"
+        >
+          Dropdown
+        </div>
+
+        <div
+          :class="{ block: isHover, hidden: !isHover }"
+          class="absolute top-14"
+        >
+          <ul class="flex flex-col items-center justify-center">
+            <li class="text-center py-2 w-full hover:bg-gray-200">
+              Hybrid Class
+            </li>
+            <li class="text-center py-2 w-full hover:bg-gray-200">
+              Fundamental Class
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+      isHover: false,
+    }
+  },
+
+  methods: {
+    dropdown() {
+      if (this.isHover === false) {
+        this.isHover = true
+      } else this.isHover = false
+
+      console.log(this.isHover)
+    },
+  },
+  computed: {
+    checkHover() {
+      return this.isHover
+    },
+  },
+}
+</script>
+
+<style></style>
