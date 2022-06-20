28ddea1cad32bfa2d61cd828c7b3e105ab4cd3cf
drigoalexander <drigosihombinga@gmail.com>
2022-06-20 19:12:55 +0700

    test aja

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,9 +11,7 @@
 <script>
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
-export default {
-  components: { Navbar, Footer },
-}
+export default { components: { Navbar, Footer } }
 </script>
 
 <style></style>
