c79d41e8404e4a8b83cf7a36048a48e7d13a0815
drigoalexander <drigosihombinga@gmail.com>
2022-07-02 14:27:09 +0700

    add privacy, edit profile , transition page

---
+++ layouts/privacy.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="w-full h-auto mx-auto flex flex-col gap-6 max-w-screen-2xl font-poppins bg-white"
+  >
+    <Navbar />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+import Footer from '~/components/Footer.vue'
+export default {
+  components: { Navbar, Footer },
+}
+</script>
+
+<style></style>
---
+++ pages/User/privacy-policy.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- Start Content -->
+
+  <div class="w-full px-10 pb-32 pt-10 bg-white">
+    <div class="flex flex-col gap-10">
+      <h1 class="text-3xl font-semibold">Privacy Policy C-Loyal</h1>
+      <div class="flex flex-col gap-8">
+        <div class="flex flex-col gap-4">
+          <h4 class="text-xl font-semibold">Kebijakan privasi C-Loyal</h4>
+          <p>
+            C-Loyal menetapkan Kebijakan Privasi ini untuk membantu pengguna
+            membuat keputusan yang jelas tentang apakah menggunakan atau tetap
+            menggunakan Layanan C-Loyal. Jika pengguna tidak setuju dengan
+            praktek C-Loyal, mohon untuk tidak menggunakan LayananC-Loyal.
+            Penggunaan atas Layanan C-Loyal dan setiap informasi pribadi yang
+            diberikan oleh Pengguna pada Layanan C-Loyal senantiasa tunduk pada
+            syarat dan ketentuan Kebijakan Privasi. Mohon dicatat bahwa
+            pengiriman konten oleh pengguna atau konten lainnya yang
+            dipublikasikan atas arahan atau kebijaksanaan para pengguna dari
+            Layanan C-loyal menjadi konten yang dipublikasikan dan tidak
+            dianggap sebagai informasi yang dapat teridentifikasi secara pribadi
+            tunduk pada Kebijakan Privasi ini.
+          </p>
+        </div>
+
+        <div class="flex flex-col gap-4">
+          <h4 class="text-xl font-semibold">
+            Apa saja yang diatur dalam kebijakan ini?
+          </h4>
+          <p>
+            Kebijakan privasi ini meliputi perlakukan terhadap informasi
+            pengguna, termasuk informasi identitas pribadi, yang diperoleh oleh
+            C-Loyal, termasuk informasi yang diperoleh saat pengguna menggunakan
+            Layanan C-Loyal yang disediakan oleh C-Loyal. Kebijakan Privasi ini
+            tidak berlaku untuk praktek perusahaan yang tidak dimiliki atau
+            tidak dikendalikan oleh C-Loyal, atau untuk individu yang tidak
+            dipekerjakan atau dipimpin oleh C-Loyal, termasuk pihak ketiga yang
+            kepadanya C-Loyal dapat mengungkap informasi pengguna sebagaimana
+            ditetapkan dalam Kebijakan Privasi ini.
+          </p>
+        </div>
+
+        <div class="flex flex-col gap-4">
+          <h4 class="text-xl font-semibold">Judul</h4>
+          <p>
+            C-Loyal menetapkan Kebijakan Privasi ini untuk membantu pengguna
+            membuat keputusan yang jelas tentang apakah menggunakan atau tetap
+            menggunakan Layanan C-Loyal. Jika pengguna tidak setuju dengan
+            praktek C-Loyal, mohon untuk tidak menggunakan LayananC-Loyal.
+            Penggunaan atas Layanan C-Loyal dan setiap informasi pribadi yang
+            diberikan oleh Pengguna pada Layanan C-Loyal senantiasa tunduk pada
+            syarat dan ketentuan Kebijakan Privasi. Mohon dicatat bahwa
+            pengiriman konten oleh pengguna atau konten lainnya yang
+            dipublikasikan atas arahan atau kebijaksanaan para pengguna dari
+            Layanan C-loyal menjadi konten yang dipublikasikan dan tidak
+            dianggap sebagai informasi yang dapat teridentifikasi secara pribadi
+            tunduk pada Kebijakan Privasi ini.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- End of Bar Question -->
+</template>
+
+<script>
+export default {
+  layout: 'privacy',
+}
+</script>
