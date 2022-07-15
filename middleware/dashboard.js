673d6085c0b1b96542aa804a19b8f354a2d9f510
drigoalexander <drigosihombinga@gmail.com>
2022-07-16 04:25:33 +0700

    try redundant fix

--- middleware/dashboard.js
+++ middleware/dashboard.js
@@ -1,8 +1,13 @@
-export default function ({ redirect, app }) {
+export default function ({ redirect, app, context }) {
   if (app.$cookies.get('role') !== 'User' && app.$cookies.get('token')) {
     redirect('/admin/dashboard')
   }
   if (!app.$cookies.get('role')) {
     redirect('/')
+  }
+
+  if (context.route.name === 'index') {
+    // skip middleware
+    return
   }
 }
